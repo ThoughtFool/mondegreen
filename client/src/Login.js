737f63e00259046e6c562d6baa1e794e9ba2d83d
ThoughtFool <thotfool@gmail.com>
2021-08-08 04:03:27 -0400

    fixing  login page

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,15 +1,12 @@
-import dontenv from "donenv";
 import React from "react";
 import { Container } from "react-bootstrap";
 
-dontenv.config();
 // import redirect_uri from "./config/"
 
 const client_id = "11811ab54cf1454bbfa5075f5ed86dba";
 const response_type = "code";
 const redirect_uri =
-    "http://localhost:3000" ||
-    process.env.REDIRECT_URI;
+    "http://localhost:3000" || "https://monde-green.herokuapp.com/";
 const scope = [
     "streaming",
     "user-read-email",
